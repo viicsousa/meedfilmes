c86750189730b776e35197978bb18bf060c3fa74
Victoria <victoria.vasconcelos@hotmail.com>
2023-03-02 14:55:34 -0300

    feat: :art: finished navbar

--- src/components/NavBar/index.page.tsx
+++ src/components/NavBar/index.page.tsx
@@ -1,11 +1,31 @@
+import Router, { useRouter } from "next/router";
 import Lottie from "react-lottie";
 
-import { Container } from "./styles";
+import { Container, Logo, Title, NavHome, Subtitle } from "./styles";
+
+import Films from "../../assets/animations/filmes.json";
 
 export default function NavBar () {
+  const defaultOptionsFilmsAnimation = {
+    loop: false,
+    autoplay: true, 
+    animationData: Films,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    },
+  };
+
+  const router = useRouter();
   return (
     <Container>
-      
+      <Logo>
+        <Lottie options={defaultOptionsFilmsAnimation} height={70} width={180}/>
+        <Title>MEED FILMES</Title>
+        </Logo>
+        <NavHome>
+          <Subtitle onClick={() => (router.push("/homePage"))}>HOME</Subtitle>
+          <Subtitle>SEARCH</Subtitle>
+        </NavHome>
     </Container>
   )
 }
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -4,4 +4,36 @@ export const Container = styled.div`
   background-color: #86BFC2;
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+ 
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  font-weight: 600;
+  font-size: 22px;
+  margin: 0;
+  
+`;
+
+export const NavHome = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 0 1rem;
+`;
+
+export const Subtitle = styled.div`
+  cursor: pointer;
+  font-weight: 600;
+
+  :hover {
+    opacity: 0.7;
+  }
 `;
