8217f0867289af25961953f7de79433dfc16a3d0
Victoria <victoria.vasconcelos@hotmail.com>
2023-03-03 14:21:15 -0300

    feat: :art:

--- src/pages/homePage/index.page.tsx
+++ src/pages/homePage/index.page.tsx
@@ -1,11 +1,46 @@
-import { Structure} from './styles';
+import { useState } from 'react';
+
+import Image from 'next/image';
+
+import { Structure, ContainerNow, Title, Cards, Card} from './styles';
+
+import Arrowright from "../../assets/svg/arrow-right.svg";
+import Arrowleft from "../../assets/svg/arrow-left.svg";
 
 import NavBar from '../../components/NavBar/index.page';
 
 export default function HomePage () {
+  const [scrollX, setScrollX] = useState(0);
+    const cliqueSetaEsquerda = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+            if(x > 0){
+                x = 0;
+            }
+        setScrollX(x);
+    }
+    const cliqueSetaDireita = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let tamanhoCompletoLista = Card.results;
+            if((window.innerWidth - tamanhoCompletoLista) > x) {
+                x = (window.innerWidth - tamanhoCompletoLista) - 60;
+            }
+        setScrollX(x);
+    }
+
   return (
     <Structure>
       <NavBar/>
+      <ContainerNow>
+        <Title></Title>
+        <Cards>
+          <Image src={Arrowleft} alt="" className='arrow' height={50} onClick={cliqueSetaEsquerda}/>
+          <Card >
+            
+          </Card>
+          
+          <Image src={Arrowright} alt="" className='arrow' height={50} onClick={cliqueSetaDireita}/>
+        </Cards>
+      </ContainerNow>
     </Structure>
   )
 }
--- src/pages/homePage/styles.ts
+++ src/pages/homePage/styles.ts
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
 export const Structure = styled.div``;
+
+export const ContainerNow = styled.div`
+  background-color: black;
+  padding: 1rem;
+`;
+
+export const Title= styled.h3``;
+
+export const Cards = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  position: relative;
+  .arrow {
+    cursor: pointer;
+  }
+`;
+
+export const Card = styled.div`
+  background-color: red;
+  width: 40rem;
+  height: 10rem;
+`;
